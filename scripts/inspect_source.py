63c487cad819bed0aaa3711b748a4791e2fd315b
Muckds89 <destavola.marco@gmail.com>
2026-09-08 13:30:36 +0200

    normalisation process phase 2

---
+++ scripts/inspect_source.py
@@ -0,0 +1,26 @@
+import requests
+import urllib
+
+# get key value and key type of a json response from a url
+def get_json_response(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+        json_data = response.json()
+        fields_dict = {key: type(value) for key, value in json_data.items()}
+        list_field = [key for key, value in fields_dict.items() if value == list]
+        name_value_dict = {}
+        for key, value in json_data.items():
+            if isinstance(value, list):
+                for item in value:
+                    for sub_key, sub_value in item.items():
+                        name_value_dict[sub_key] = type(sub_value)
+                name_value_dict[key] = type(json_data[key])
+        return list_field, name_value_dict
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from {url}: {e}")
+        return None
+    except ValueError as e:
+        print(f"Error parsing JSON from {url}: {e}")
+        return None, None
+
